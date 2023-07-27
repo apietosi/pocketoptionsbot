6a6fe74ba14eacf6233f10d30e813e8b0d3a6b1c
apietosi <35179623+apietosi@users.noreply.github.com>
2023-07-27 15:18:44 -0400

    Add files via upload

--- content_script_injector.js
+++ content_script_injector.js
@@ -2,6 +2,6 @@ console.log('injecting content script');
 var s = document.createElement('script');
 s.src = chrome.runtime.getURL('content_script.js');
 s.onload = function() {
-  this.parentNode.removeChild(this);
+    this.parentNode.removeChild(this);
 };
-(document.head || document.documentElement).appendChild(s);
+(document.head||document.documentElement).appendChild(s);
